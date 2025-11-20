76e377c055b2604291c991d3234672b7daf2c046
Daniel Lopez <daniel.lopez@comercialcard.com.co>
2025-11-20 09:03:57 -0500

    Se corrige el edit de partners group, se crean las vistas  y templates para editar y eliminar partners

--- partners/urls.py
+++ partners/urls.py
@@ -5,6 +5,8 @@ app_name = 'partners'
 
 urlpatterns = [
     path('', views.show, name='show'),
-    path('create', views.create, name='create')
+    path('create', views.create, name='create'),
+    path('delete/<int:id>', views.delete, name='delete'),
+    path('edit/<int:id>', views.edit, name='edit'),
 ]
 
--- partners/views.py
+++ partners/views.py
@@ -29,4 +29,30 @@ def create(request):
         form = PartnerForm()
     return render(request, "partners/create.html", {"form": form})
 
-# Create your views here.
+def delete(request, id):
+    print("llego acá")
+    partner = Partner.objects.get(id=id)
+    partner.delete()
+    return redirect("partners:show")
+
+def edit(request, id):
+    partner = Partner.objects.get(id=id)
+    if request.method == "POST":
+        form = PartnerForm(request.POST)
+        if form.is_valid():
+            partner.full_name = form.cleaned_data['full_name']
+            partner.email = form.cleaned_data['email']
+            partner.cellphone = form.cleaned_data['cellphone']
+            partner.partner_group = form.cleaned_data['partner_group']
+            partner.save()
+            return redirect("partners:show")
+    else:
+        form = PartnerForm(initial={
+            'full_name': partner.full_name,
+            'email': partner.email,
+            'cellphone': partner.cellphone,
+            'partner_group': partner.partner_group
+        })
+
+    return render(request, "partners/edit.html", {"form": form})
+
