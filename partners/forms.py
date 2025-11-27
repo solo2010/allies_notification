53c6b1806807e3336ef36aaf0f5edb608ee32416
Daniel Lopez <daniel.lopez@comercialcard.com.co>
2025-11-27 11:31:39 -0500

    Se modifica el modelo de partners para que email sea campo único, se agrega span para mostrar errores del formulario

--- partners/forms.py
+++ partners/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from partner_groups.models import PartnerGroup
+from .models import Partner
 
 class PartnerForm(forms.Form):
     full_name = forms.CharField(label="Nombre Completo",max_length=100)
@@ -9,3 +10,9 @@ class PartnerForm(forms.Form):
         queryset=PartnerGroup.objects.all(),
         empty_label="Seleccione el grupo aliado"
     )
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if Partner.objects.filter(email=email).exists():
+            raise forms.ValidationError("Esta email ya está registrado.")
+        return email
--- partners/models.py
+++ partners/models.py
@@ -4,7 +4,7 @@ from partner_groups.models import PartnerGroup
 class Partner(models.Model):
     partner_group = models.ForeignKey(PartnerGroup, on_delete=models.CASCADE, related_name="partners")
     full_name = models.CharField(max_length=255)
-    email = models.EmailField()
+    email = models.EmailField(unique=True)
     cellphone = models.CharField(max_length=25)
 
 
