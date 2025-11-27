23bc32c006a5ad061391adddd8124f02953ea70f
Daniel Lopez <daniel.lopez@comercialcard.com.co>
2025-11-27 15:22:21 -0500

    Se crea y se migra el model de mails

--- mails/models.py
+++ mails/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from products.models import Product
+from statuses.models import Status
 
-# Create your models here.
+class Mail(models.Model):
+    product = models.ForeignKey(Product,on_delete=models.SET_NULL, null=True, related_name='mails')
+    status = models.ForeignKey(Status, on_delete=models.SET_NULL, null=True, related_name='mails')
+    recipients = models.JSONField(default=list, help_text="Lista de Correos con estado de envio")
+    subject = models.CharField(max_length=500)
+    contet = models.TextField()
+    creation_date = models.DateTimeField(auto_now_add=True, null=True)
+    update_date = models.DateTimeField(auto_now=True, null=True)
